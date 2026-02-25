6460ad4a8d2d52c8fc06423829037fc0d14ef153
Yuxuan Ma <mayuxuan2004@163.com>
2026-02-25 12:44:15 +0000

    sim2sim is successful

--- mujoco_playground/_src/locomotion/go2/go2_constants.py
+++ mujoco_playground/_src/locomotion/go2/go2_constants.py
@@ -45,3 +45,5 @@ ANCHOR_OBS_DIM = 40
 ANCHOR_ACT_DIM = 12
 ANCHOR_PATH = '/data/mujoco_playground/mujoco_playground/experimental/learning/checkpoints/Go2Trot-20260201-141932/trotting_apg_2hz_policy.pkl'
 
+RESIDUAL_OBS_DIM = 48
+RESIDUAL_ACT_DIM = 12
